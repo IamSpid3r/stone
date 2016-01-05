0c322a5ee3ac5dd68d9c0e89ce533f1a0bc48689
root <root@symfonywangpeng-8-179-dev.sh.hupu.com>
2016-01-05 17:47:53 +0800

    修改：     index.js

--- index.js
+++ index.js
@@ -11,15 +11,27 @@ var compress = require('compression');
 // var url = require('url');
 var taobao = require('./lib/taobao');
 var amazon = require('./lib/amazon');
+var nikeStore = require('./lib/nikeStore');
+
 app.use(compress());
 app.get('/test', function (req, res) {
-    // taobao.getInfo('https://item.taobao.com/item.htm?spm=a230r.1.14.20.EFhUKi&id=45122936450',function(error,itemInfo){
-    //     res.send(itemInfo);
-    // })
-    amazon.getInfo('http://www.amazon.cn/gp/product/B013OOT614/ref=s9_cngwdyfloorv2-s9?pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=desktop-1&pf_rd_r=0NXVQ726G3DZPAQBVMTQ&pf_rd_t=36701&pf_rd_p=B013OOT614&pf_rd_i=desktop',function(error,itemInfo){
+    /* taobao.getInfo('https://item.taobao.com/item.htm?spm=a230r.1.14.20.EFhUKi&id=45122936450',function(error,itemInfo){
+         if(error){
+             res.send(error);
+         }else{
+             res.send(itemInfo);
+         }
+     })*/
+   /* amazon.getInfo('http://www.amazon.cn/gp/product/B013OOT614/ref=s9_cngwdyfloorv2-s9?pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=desktop-1&pf_rd_r=0NXVQ726G3DZPAQBVMTQ&pf_rd_t=36701&pf_rd_p=B013OOT614&pf_rd_i=desktop',function(error,itemInfo){
         res.send(itemInfo);
+    })*/
+    nikeStore.getInfo('http://store.nike.com/cn/zh_cn/pd/air-max-2016-%E8%B7%91%E6%AD%A5%E9%9E%8B/pid-10865050/pgid-10345833',function(error,itemInfo){
+         if(error){
+             res.send(error);
+         }else{
+             res.send(itemInfo);
+         }
     })
-
 
 })
 app.listen(3000)
