97998377647416f74ac03921cff9aeaecaf03068
hanxiaolin <724753832@qq.com>
2016-01-14 16:53:43 +0800

    6pm抓取 and 识货抓取

--- index.js
+++ index.js
@@ -14,19 +14,21 @@ var taobao = require('./lib/taobao');
 var amazonCn = require('./lib/amazonCn');
 var nikeStore = require('./lib/nikeStore');
 var yougou = require('./lib/yougou');
+var _6pm = require('./lib/6pm');
+var shihuoHaitao = require('./lib/shihuoHaitao');
 
 app.use(compress());
 app.use(bodyParser.json());
 
 
 app.get('/test', function (req, res) {
-   taobao.getInfo('https://item.taobao.com/item.htm?spm=a230r.1.14.20.EFhUKi&id=45122936450',function(error,itemInfo){
+  /* taobao.getInfo('https://item.taobao.com/item.htm?spm=a230r.1.14.20.EFhUKi&id=45122936450',function(error,itemInfo){
          if(error){
              res.send(error);
          }else{
              res.send(itemInfo);
          }
-     })
+     })*/
    /* amazon.getInfo('http://www.amazon.cn/gp/product/B013OOT614/ref=s9_cngwdyfloorv2-s9?pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=desktop-1&pf_rd_r=0NXVQ726G3DZPAQBVMTQ&pf_rd_t=36701&pf_rd_p=B013OOT614&pf_rd_i=desktop',function(error,itemInfo){
         res.send(itemInfo);
     })*/
@@ -46,9 +48,27 @@ app.get('/test', function (req, res) {
         }
     })*/
 
+    _6pm.getInfo('http://www.6pm.com/product/8550462/color/567398',function(error,itemInfo){
+        if(error){
+            res.send(error);
+        }else{
+            res.send(itemInfo);
+        }
+    })
+
+  /*  shihuoHaitao.getInfo('http://www.shihuo.cn/haitao/buy/84756.html?from=shyjg',function(error,itemInfo){
+        if(error){
+            res.send(error);
+        }else{
+            res.send(itemInfo);
+        }
+    })*/
 })
 
-var allowHostname = ['www.amazon.cn', 'item.taobao.com', 'detail.tmall.com', 'store.nike.com', 'www.yougou.com', 'seoul.yougou.com'];
+var allowHostname = [
+    'www.amazon.cn', 'item.taobao.com', 'detail.tmall.com','www.shihuo.cn',
+    'store.nike.com', 'www.yougou.com', 'seoul.yougou.com','www.6pm.com'
+];
 app.get('/info', function (req, res) {
     var goodsUrl = req.query.url;
     var goodsUrlHost = '';
@@ -110,6 +130,30 @@ app.get('/info', function (req, res) {
         case 'www.yougou.com':
         case 'seoul.yougou.com':
             yougou.getInfo(goodsUrl ,function(error, itemInfo){
+                if(error){
+                    res.json({
+                        Status: false,
+                        Msg: error
+                    })
+                }else{
+                    res.json({ Status: true, Data: itemInfo});
+                }
+            })
+            break;
+        case 'www.shihuo.cn':
+            shihuoHaitao.getInfo(goodsUrl ,function(error, itemInfo){
+                if(error){
+                    res.json({
+                        Status: false,
+                        Msg: error
+                    })
+                }else{
+                    res.json({ Status: true, Data: itemInfo});
+                }
+            })
+            break;
+        case 'www.6pm.com':
+            _6pm.getInfo(goodsUrl ,function(error, itemInfo){
                 if(error){
                     res.json({
                         Status: false,
