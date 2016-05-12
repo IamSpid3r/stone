85da84d5db7e955029560988e58f13fbf62cc3a2
hanxiaolin <724753832@qq.com>
2016-05-12 10:51:34 +0800

    修改

---
+++ test/jd.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+var jd = require('../lib/jd');
+
+
+describe('jd', function() {
+    describe('#getItemInfo', function () {
+        it('测试jd商品',function(done){
+            this.timeout(60000)
+            jd.getInfo('http://item.jd.com/2644110.html',function(err,data){
+                if(err)
+                {
+                    throw new Error(err);
+                }
+                assert.equal('inStock',data.Status);
+                assert.equal('cn.jd.2644110',data.Unique);
+                assert.ok(data.Variations.length > 0,'data.Variations.length is 0');
+                assert.ok(data.Variations[0].Values.length > 0,'data.Variations[0].Values is 0');
+                assert.ok(data.Items.length > 0,'data.Items.length is 0')
+                assert.ok(data.Items[0].Attr.length > 0,'data.Items[0].Attr.length is 0')
+                assert.ok(data.Items[0].Offers.length > 0,'data.Items[0].Attr[0].Offers.length is 0')
+                assert.ok(data.Items[0].Offers[0].List[0].Price > 0,'data.Items[0].Attr[0].Offers[0].list[0].price is 0')
+                done()
+            })
+        })
+
+        it('测试jd下架商品',function(done){
+            this.timeout(60000)
+            jd.getInfo('http://item.jd.com/1314491788.html',function(err,data){
+                if(err)
+                {
+                    throw new Error(err);
+                }
+                assert.equal('outOfStock',data.Status);
+                done()
+            })
+        })
+
+        it('测试jd未找到商品',function(done){
+            this.timeout(60000)
+            jd.getInfo('http://item.jd.com/1540216436.html',function(err,data){
+                if(err)
+                {
+                    throw new Error(err);
+                }
+                assert.equal('notFind',data.Status);
+                done()
+            })
+        })
+    });
+
+});
