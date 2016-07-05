084daca3c60e59db666bf608af7be0244f327d0e
unknown <gupenghui@sh.hupu.com>
2016-07-05 17:26:51 +0800

    安德玛修改

---
+++ test/xtep.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var yohobuy = require('../lib/xtep');
+
+
+describe('xtep', function() {
+    describe('#getItemInfo', function () {
+        it('测试xtep正常商品',function(done){
+            this.timeout(30000)
+            yohobuy.getInfo('http://www.xtep.com.cn/goods/do_index?goods_sn=984219119511',function(err,data){
+                if(err)
+                {
+                    throw new Error(err);
+                }
+                assert.equal('inStock',data.Status);
+                assert.equal('cn.underarmour.984219119511',data.Unique);
+                assert.ok(data.Variations.length > 0,'data.Variations.length is 0');
+                assert.ok(data.Variations[0].Values.length > 0,'data.Variations[0].Values is 0');
+                assert.ok(data.Items.length > 0,'data.Items.length is 0')
+                assert.ok(data.Items[0].Attr.length > 0,'data.Items[0].Attr.length is 0')
+                assert.ok(data.Items[0].Offers.length > 0,'data.Items[0].Attr[0].Offers.length is 0')
+                assert.ok(data.Items[0].Offers[0].List[0].Price > 0,'data.Items[0].Attr[0].Offers[0].list[0].price is 0')
+                done()
+            })
+        })
+    });
+
+});
