22d44b4ef8e6098748cbd6cc6d212bf7ec650152
gwyy <279861795@qq.com>
2017-08-08 10:00:51 +0800

    非标类详情抓取

---
+++ supplierDetail.js
@@ -0,0 +1,120 @@
+var fs = require('fs');
+var express = require('express'), app = express();
+var server = require('http').createServer(app);
+var compress = require('compression');
+var bodyParser = require('body-parser');
+var url = require('url');
+var request = require('request');
+var domain = require('domain');
+
+var taobao = require('./lib/supplierDetail/tbDetail');
+var yohobuy = require('./lib/supplierDetail/yohobuyDetail');
+var jd = require('./lib/supplierDetail/jdDetail');
+
+app.use(compress());
+app.use(bodyParser.json());
+app.use(express.static('mochawesome-reports'));
+
+app.get('/test', function (req, res) {
+
+})
+
+app.use(function (req, res, next) {
+    var reqDomain = domain.create();
+    reqDomain.on('error', function (e) {
+        try {
+            var killTimer = setTimeout(function () {
+                process.exit(1);
+            }, 30000);
+            killTimer.unref();
+            server.close();
+
+            var stack = JSON.stringify(e.stack);
+            res.json({
+                Status: false,
+                Msg: {
+                    Errors: {
+                        Code: 'Error',
+                        Message: stack.slice(0, 120)
+                    }
+                }
+            });
+        } catch (e) {
+            console.log('error when exit', e.stack);
+        }
+    });
+
+    reqDomain.run(next);
+});
+
+
+
+app.get('/info', function (req, res) {
+    var goodsUrl = req.query.url;
+    var urlInfo = goodsUrl ?  url.parse(goodsUrl, true, true) : {path:'',host:''};
+
+    var storeObj = getStoreObj(urlInfo);
+    if(typeof storeObj == 'object'){
+        storeObj.getInfo(encodeURI(goodsUrl) ,function(error, itemInfo){
+            if(error){
+                res.json({
+                    Status: false,
+                    Msg: error
+                }).end();
+            }else{
+                res.json({ Status: true, Data: itemInfo}).end();
+            }
+        })
+    }else{
+        res.json({
+            Status: false,
+            Msg: {
+                Errors: {
+                    Code: 'Fatal',
+                    Message: '当前地址不支持爬取'
+                }
+            }
+        }).end();
+    }
+})
+
+
+
+
+// uncaughtException 避免程序崩溃
+process.on('uncaughtException', function (err) {
+    console.log('uncaughtException->',err);
+
+    try {
+        var killTimer = setTimeout(function () {
+            process.exit(1);
+        }, 30000);
+        killTimer.unref();
+
+        server.close();
+    } catch (e) {
+        console.log('error when exit', e.stack);
+    }
+});
+
+//获取商城对象
+function getStoreObj(urlInfo){
+    switch(urlInfo.host){
+        case 'item.taobao.com':
+        case 'detail.tmall.com':
+        case 'detail.tmall.hk':
+            return taobao;
+        case 'item.yohobuy.com':
+            return yohobuy;
+        case 'item.jd.com':
+        case 'item.jd.hk':
+            return jd;
+        default:
+            return '';
+    }
+}
+
+app.listen(3003,function(){
+   console.log('listen 3303');
+})
+
