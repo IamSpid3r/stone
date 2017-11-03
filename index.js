80dd80e2607f1c47fd676e7dd247c22632ee68f7
韩晓林 <hanxiaolin@hupu.com>
2017-11-03 18:56:22 +0800

    s11taobao

--- index.js
+++ index.js
@@ -359,7 +359,6 @@ app.get('/qqq', function (req, res) {
         req.headers['x-forwarded-for'] ,
         req.connection.remoteAddress ,
         req.socket.remoteAddress ,
-        req.connection.socket.remoteAddress
     ]}).end();
 })
 
