d0afdebd0e9feee3bcd29e0c2eeb539421f0402b
ddtey <540501185@qq.com>
2018-02-07 11:21:39 +0800

    crawlmain 迁移es

--- apps/evolution/crawlMainNotice.js
+++ apps/evolution/crawlMainNotice.js
@@ -4,16 +4,16 @@ const Op = SequelizeDb.sequelize.Op;
 var url  = require('url');
 var fun  = require('../lib/fun');
 const tableStore = require(process.cwd()+"/apps/lib/tablestorecrawlcontent.js").tableStore;
-
+const crawlmainTaskES = require(process.cwd()+"/apps/lib/elasticsearch/crawlMainTasks.js").esClient;
 const Q = require("q");
 
 
 var deal = function(){
     console.log('start notice')
-	controller.getDataList(200).then(function (data) {
+	controller.getDataListEs(200).then(function (data) {
             if (data.status){
                 data.data.forEach(function (row) {
-                    if (row.id){
+                    if (row.task_id){
                         //通知给晓林
                         var skuInfo = row.sku_info;
                         if (!skuInfo){
@@ -28,14 +28,14 @@ var deal = function(){
                                     }
                                     receiveQueue.handler(row.task_id, row.url,  skuInfo, function(error, info){
                                         if(error){
-                                            console.log(row.id+' callback error');
+                                            console.log(row.task_id+' callback error');
                                             fun.stoneLog('crawlMainNotice', 'error', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice error--'+error}})
                                             //失败
-                                            controller.updateDataError(row.id,parseInt(row.callback_err_num)+1).then(function (data) {})
+                                            controller.updateDataErrorEs(row.task_id,parseInt(row.callback_err_num)+1).then(function (data) {})
                                         } else {
                                             //成功
-                                            controller.updateDataSuccess(row.id).then(function (data) {
-                                                console.log(row.id+' callback success');
+                                            controller.updateDataSuccessEs(row.task_id).then(function (data) {
+                                                console.log(row.task_id+' callback success');
                                                 fun.stoneLog('crawlMainNotice', 'info', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice success'}})
                                             },function (err) {
                                                 fun.stoneLog('crawlMainNotice', 'error', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice error--'+err.message}})
@@ -47,22 +47,22 @@ var deal = function(){
                             })
                         }else{
                             if (!fun.isJson(skuInfo)){
-                                    console.log(row.id+' callback json error');
+                                    console.log(row.task_id+' callback json error');
                                     fun.stoneLog('crawlMainNotice', 'error', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice json error'}})
                                     //失败
-                                    controller.updateDataError(row.id,parseInt(row.callback_err_num)+1).then(function (data) {})
+                                    controller.updateDataErrorEs(row.task_id,parseInt(row.callback_err_num)+1).then(function (data) {})
                                 } else {
                                     skuInfo = JSON.parse(skuInfo);
                                     receiveQueue.handler(row.task_id, row.url,  skuInfo, function(error, info){
                                         if(error){
-                                            console.log(row.id+' callback error');
+                                            console.log(row.task_id+' callback error');
                                             fun.stoneLog('crawlMainNotice', 'error', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice error--'+error}})
                                             //失败
-                                            controller.updateDataError(row.id,parseInt(row.callback_err_num)+1).then(function (data) {})
+                                            controller.updateDataErrorEs(row.task_id,parseInt(row.callback_err_num)+1).then(function (data) {})
                                         } else {
                                             //成功
-                                            controller.updateDataSuccess(row.id).then(function (data) {
-                                                console.log(row.id+' callback success');
+                                            controller.updateDataSuccessEs(row.task_id).then(function (data) {
+                                                console.log(row.task_id+' callback success');
                                                 fun.stoneLog('crawlMainNotice', 'info', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice success'}})
                                             },function (err) {
                                                 fun.stoneLog('crawlMainNotice', 'error', {"param1" : row.task_id, "param2":row.url, "param":{"message":'notice error--'+err.message}})
@@ -167,6 +167,79 @@ var controller = {
         ).catch(err => {
             return defer.reject(err);
         });
+        return defer.promise;
+    },
+    getDataListEs:function(pagesize){
+        var defer = Q.defer();
+        
+        crawlmainTaskES.search(
+            { callback_status: 0, callback_err_num: 5,notice:1, size: pagesize, sort: [['update_at', 'asc']]
+        }, function (err, res) {
+            if (err) {
+                return defer.reject(err);
+            }
+
+            var data;
+            var rows = res.hits.hits;
+            console.log(rows)
+            return;
+            var data = [];
+            rows.forEach(function (row) {
+                data.push({
+                    task_id: row._source.task_id,
+                    update_err_num: row._source.update_err_num,
+                    url:row._source.url,
+                    sku_info:row._source.sku_info,
+                    callback_err_num:row._source.callback_err_num
+                })
+            })
+
+            if (rows.length > 0){
+                return defer.resolve({
+                    status : true,
+                    data:data
+                });
+            } else {
+                return defer.reject('没有任务了');
+            }
+        })
+        return defer.promise;
+    },
+    updateDataErrorEs:function(task_id,err_num){
+        var defer = Q.defer();
+        var now = new Date();
+
+        crawlmainTaskES.update({
+            task_id: taskId,
+            'callback_status' : 0,
+            'callback_err_num' : err_num
+        }, function (err, res) {
+            if (err) {
+                return defer.reject(new Error('保存ES错误'));
+            }
+
+            return defer.resolve({
+                status : true
+            });
+        })
+        return defer.promise;
+    },
+    updateDataSuccessEs:function(task_id){
+        var defer = Q.defer();
+        var now = new Date();
+
+        crawlmainTaskES.update({
+            task_id: taskId,
+            'callback_status' : 1
+        }, function (err, res) {
+            if (err) {
+                return defer.reject(new Error('保存ES错误'));
+            }
+
+            return defer.resolve({
+                status : true
+            });
+        })
         return defer.promise;
     }
 }
