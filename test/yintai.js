5d530dd3e5eef220b3feb3645c910051b335345e
韩晓林 <hanxiaolin@hupu.com>
2018-02-26 14:59:45 +0800

    evolution

--- test/yintai.js
+++ test/yintai.js
@@ -6,13 +6,13 @@ describe('yintai', function() {
     describe('#getItemInfo', function () {
         it('测试yintai正常商品',function(done){
             this.timeout(30000)
-            yintai.getInfo('http://item.yintai.com/21-178-6689C.html',function(err,data){
+            yintai.getInfo('http://item.yintai.com/21-354-7885C.html?from=bfd_b',function(err,data){
                 if(err)
                 {
                     throw new Error(err);
                 }
                 assert.equal('inStock',data.Status);
-                assert.equal('cn.yintai.21.178.6689',data.Unique);
+                assert.equal('cn.yintai.21.354.7885',data.Unique);
                 assert.ok(data.Variations.length > 0,'data.Variations.length is 0');
                 assert.ok(data.Variations[0].Values.length > 0,'data.Variations[0].Values is 0');
                 assert.ok(data.Items.length > 0,'data.Items.length is 0')
