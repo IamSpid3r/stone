af02c47887d42c80d2c8ec86a2dcbf9dbf8975d4
ddtey <540501185@qq.com>
2018-03-20 15:49:02 +0800

    get task update

--- apps/evolution/getCrawlTask.js
+++ apps/evolution/getCrawlTask.js
@@ -96,249 +96,156 @@ exports.getMainList = function(request, response) {
 }
 
 //处理核心逻辑
-var handler = function (request, response){
-	var store = request.query.store;
-    //验证 必须传store
-    if (!store) {
-        response.json({code: 400, msg: '缺少store参数'});
-        return;
-    }
+// var handler = function (request, response){
+// 	var store = request.query.store;
+//     //验证 必须传store
+//     if (!store) {
+//         response.json({code: 400, msg: '缺少store参数'});
+//         return;
+//     }
 
-    if (_.findIndex(storeArr, {'name':store}) != -1){//是否是需要单独获取的商城
-    	redlock.lock('stone_get_crawl_task1_'+ store, 4).done(//锁3秒
-		  function(lock){
-		  	//拿到锁以后获取一条数据
-	    	controller.getDataEs(store).then(function (data) {
-		    	if(data){
-		    		var redis_key = 'stone_get_crawl_task1_'+data.data.task_id;
-		    		//用redis的方式存储是否获取过，防止es更新缓慢造成重复抓取的问题
-		    		client.get(redis_key, function (err, reply) {
-	                    if (!reply){//没有获取过
-	                    	//获取完把状态更新成1（处理中）
-				    		controller.updateDataEs(data.data.task_id).then(function (datas) {
-				    			client.set(redis_key, 1);
-		                    	client.expire(redis_key, 10);
-				    			redlock.unlock(lock);//释放锁
-				    			response.json({code: 200, msg: '',data:data.data});
-				    		}, function (errs) {
-						        redlock.unlock(lock);//释放锁
-						        response.json({code: 400, msg: err.message});
-						    })
-	                    } else {
-	                    	redlock.unlock(lock);//释放锁
-	                    	//100ms后继续获取
-	                    	setTimeout(function(){
-	                    		handler(request, response);
-	                    	},50)
-	                    }
-	                });
+//     if (_.findIndex(storeArr, {'name':store}) != -1){//是否是需要单独获取的商城
+//     	redlock.lock('stone_get_crawl_task1_'+ store, 4).done(//锁3秒
+// 		  function(lock){
+// 		  	//拿到锁以后获取一条数据
+// 	    	controller.getDataEs(store).then(function (data) {
+// 		    	if(data){
+// 		    		var redis_key = 'stone_get_crawl_task1_'+data.data.task_id;
+// 		    		//用redis的方式存储是否获取过，防止es更新缓慢造成重复抓取的问题
+// 		    		client.get(redis_key, function (err, reply) {
+// 	                    if (!reply){//没有获取过
+// 	                    	//获取完把状态更新成1（处理中）
+// 				    		controller.updateDataEs(data.data.task_id).then(function (datas) {
+// 				    			client.set(redis_key, 1);
+// 		                    	client.expire(redis_key, 10);
+// 				    			redlock.unlock(lock);//释放锁
+// 				    			response.json({code: 200, msg: '',data:data.data});
+// 				    		}, function (errs) {
+// 						        redlock.unlock(lock);//释放锁
+// 						        response.json({code: 400, msg: err.message});
+// 						    })
+// 	                    } else {
+// 	                    	redlock.unlock(lock);//释放锁
+// 	                    	//100ms后继续获取
+// 	                    	setTimeout(function(){
+// 	                    		handler(request, response);
+// 	                    	},50)
+// 	                    }
+// 	                });
 		    		
-		    	} else {
-		    		redlock.unlock(lock);//释放锁
-		    		response.json({code: 400, msg: err.message});
-		    	}
-		    },function (err) {
-		    	redlock.unlock(lock);//释放锁
-		        response.json({code: 400, msg: err.message});
-		    })
-		  },function(err){
-		  	//没有获取到锁则循环获取
-		  	setTimeout(function(){
-		  		handler(request, response);
-		  	},50)
-	      }); 
-    } else {//其他的商城
-		redlock.lock('stone_get_crawl_task1_'+ store, 4).done(//锁3秒
-		  function(lock){
-		  	//拿到锁以后获取一条数据
-	    	controller.getDataOtherEs(store).then(function (data) {
-		    	if(data){
-		    		var redis_key = 'stone_get_crawl_task1_'+data.data.task_id;
-		    		//用redis的方式存储是否获取过，防止es更新缓慢造成重复抓取的问题
-		    		client.get(redis_key, function (err, reply) {
-	                    if (!reply){//没有获取过
-	                    	//获取完把状态更新成1（处理中）
-				    		controller.updateDataEs(data.data.task_id).then(function (datas) {
-				    			client.set(redis_key, 1);
-		                    	client.expire(redis_key, 10);
-				    			redlock.unlock(lock);//释放锁
-				    			response.json({code: 200, msg: '',data:data.data});
-				    		}, function (errs) {
-						        redlock.unlock(lock);//释放锁
-						        response.json({code: 400, msg: err.message});
-						    })
-	                    } else {
-	                    	redlock.unlock(lock);//释放锁
-	                    	//100ms后继续获取
-	                    	setTimeout(function(){
-	                    		handler(request, response);
-	                    	},50)
-	                    }
-	                });
-		    	} else {
-		    		redlock.unlock(lock);//释放锁
-		    		response.json({code: 400, msg: err.message});
-		    	}
-		    },function (err) {
-		    	redlock.unlock(lock);//释放锁
-		        response.json({code: 400, msg: err.message});
+// 		    	} else {
+// 		    		redlock.unlock(lock);//释放锁
+// 		    		response.json({code: 400, msg: err.message});
+// 		    	}
+// 		    },function (err) {
+// 		    	redlock.unlock(lock);//释放锁
+// 		        response.json({code: 400, msg: err.message});
+// 		    })
+// 		  },function(err){
+// 		  	//没有获取到锁则循环获取
+// 		  	setTimeout(function(){
+// 		  		handler(request, response);
+// 		  	},50)
+// 	      }); 
+//     } else {//其他的商城
+// 		redlock.lock('stone_get_crawl_task1_'+ store, 4).done(//锁3秒
+// 		  function(lock){
+// 		  	//拿到锁以后获取一条数据
+// 	    	controller.getDataOtherEs(store).then(function (data) {
+// 		    	if(data){
+// 		    		var redis_key = 'stone_get_crawl_task1_'+data.data.task_id;
+// 		    		//用redis的方式存储是否获取过，防止es更新缓慢造成重复抓取的问题
+// 		    		client.get(redis_key, function (err, reply) {
+// 	                    if (!reply){//没有获取过
+// 	                    	//获取完把状态更新成1（处理中）
+// 				    		controller.updateDataEs(data.data.task_id).then(function (datas) {
+// 				    			client.set(redis_key, 1);
+// 		                    	client.expire(redis_key, 10);
+// 				    			redlock.unlock(lock);//释放锁
+// 				    			response.json({code: 200, msg: '',data:data.data});
+// 				    		}, function (errs) {
+// 						        redlock.unlock(lock);//释放锁
+// 						        response.json({code: 400, msg: err.message});
+// 						    })
+// 	                    } else {
+// 	                    	redlock.unlock(lock);//释放锁
+// 	                    	//100ms后继续获取
+// 	                    	setTimeout(function(){
+// 	                    		handler(request, response);
+// 	                    	},50)
+// 	                    }
+// 	                });
+// 		    	} else {
+// 		    		redlock.unlock(lock);//释放锁
+// 		    		response.json({code: 400, msg: err.message});
+// 		    	}
+// 		    },function (err) {
+// 		    	redlock.unlock(lock);//释放锁
+// 		        response.json({code: 400, msg: err.message});
 		        
-		    })
-		  },function(err){
-		  	//没有获取到锁则循环获取
-		  	setTimeout(function(){
-		  		handler(request, response);
-		  	},50)
-	      }); 
-    }
-    return;
-}
+// 		    })
+// 		  },function(err){
+// 		  	//没有获取到锁则循环获取
+// 		  	setTimeout(function(){
+// 		  		handler(request, response);
+// 		  	},50)
+// 	      }); 
+//     }
+//     return;
+// }
 
 var controller = {
-    getData:function(store){
-        var defer = Q.defer();
-        SequelizeDb.CrawlMain
-            .findOne({
-            		attributes: ['id', 'task_id', 'url', 'store'],
-	            	where: {store:store,status:0},
-	            	order: [['updatedAt','asc']]
-	                //[Op.or]: [{status: 0}, {status: 1}]
-             	}
-        	)
-            .then(crawlmain => {
-                if (!crawlmain){
-                    throw new Error('没有任务了');
-                } else {
-                    return defer.resolve({
-                        status : true,
-                        data:crawlmain
-                    });
-
-                }
-            }
-        ).catch(err => {
-            return defer.reject(err);
-        });
-        return defer.promise;
-    },
-    getDataOther:function(store){
-    	if(store == 'guonei'){
-    		var storeStr = _.chain(guoneiArr)
-			  .map(function(mall){
-			    return mall.name;
-			  })
-			  .value();
-	    }else if(store == 'taobao'){
-	    	var storeStr = _.chain(taobaoArr)
-			  .map(function(mall){
-			    return mall.name;
-			  })
-			  .value();
-    	} else {
-    		var storeStr = _.chain(guowaiArr)
-			  .map(function(mall){
-			    return mall.name;
-			  })
-			  .value();
-    	}
-        var defer = Q.defer();
-        SequelizeDb.CrawlMain
-            .findOne({
-            		attributes: ['id', 'task_id', 'url', 'store'],
-	            	where: {store: {[Op.in]:storeStr},status:0},
-	            	order: [['updatedAt','asc']]
-             	}
-        	)
-            .then(crawlmain => {
-                if (!crawlmain){
-                    throw new Error('没有任务了');
-                } else {
-                    return defer.resolve({
-                        status : true,
-                        data:crawlmain
-                    });
-
-                }
-            }
-        ).catch(err => {
-            return defer.reject(err);
-        });
-        return defer.promise;
-    },
-    updateData:function(id){
-        var defer = Q.defer();
-        SequelizeDb.CrawlMain
-            .findOne({where:{id:id }})
-            .then(crawlmain => {
-                if (crawlmain){
-                    crawlmain.update({
-                        status:1,
-                      }).then(row=>{
-                        if (!row) {
-                            return defer.reject(new Error('保存数据库错误'));
-                        }
-                        return defer.resolve({
-                            status : true
-                        });
-                    });
-                } else {
-                    return defer.reject(new Error('不存在此任务'));
-                }
-            }
-        ).catch(err => {
-            return defer.reject(err);
-        });
-        return defer.promise;
-    },
     getDataEs:function(store){
-    	var that = this;
+        var that = this;
         var defer = Q.defer();
         
         crawlmainTaskES.search(
-            { store:store, status: 0, size: 1, sort: [['from', 'desc'],['update_at', 'asc']]
+            { store:store, status: 0, size: 200, sort: [['from', 'desc'],['update_at', 'asc']]
         }, function (err, res) {
             if (err) {
                 return defer.reject(err);
             }
 
-            var data;
+            var data = [];
             var rows = res.hits.hits;
             if (rows.length > 0){
-            	data = {
-                    task_id: rows[0]._source.task_id,
-                    url: rows[0]._source.url,
-                    store : rows[0]._source.store
-                };
+                rows.forEach(function(row){
+                    data.push({
+                        task_id: row._source.task_id,
+                        url: row._source.url,
+                        store : row._source.store
+                    });
+                })
             return defer.resolve({
                     status : true,
                     data:data
                 });
-        	} else {
-        		return defer.reject('没有任务了');
-        	}
+            } else {
+                return defer.reject('没有任务了');
+            }
         })
         return defer.promise;
     },
     getDataOtherEs:function(store){
-    	if(store == 'guonei'){
-    		var storeStr = _.chain(guoneiArr)
-			  .map(function(mall){
-			    return mall.name;
-			  })
-			  .value();
-	    }else if(store == 'taobao'){
-	    	var storeStr = _.chain(taobaoArr)
-			  .map(function(mall){
-			    return mall.name;
-			  })
-			  .value();
-    	} else {
-    		var storeStr = _.chain(guowaiArr)
-			  .map(function(mall){
-			    return mall.name;
-			  })
-			  .value();
-    	}
+        if(store == 'guonei'){
+            var storeStr = _.chain(guoneiArr)
+              .map(function(mall){
+                return mall.name;
+              })
+              .value();
+        }else if(store == 'taobao'){
+            var storeStr = _.chain(taobaoArr)
+              .map(function(mall){
+                return mall.name;
+              })
+              .value();
+        } else {
+            var storeStr = _.chain(guowaiArr)
+              .map(function(mall){
+                return mall.name;
+              })
+              .value();
+        }
         var defer = Q.defer();
 
         crawlmainTaskES.search(
@@ -351,7 +258,7 @@ var controller = {
             var data;
             var rows = res.hits.hits;
             if (rows.length > 0){
-            	data = {
+                data = {
                     task_id: rows[0]._source.task_id,
                     url: rows[0]._source.url,
                     store : rows[0]._source.store
@@ -360,9 +267,9 @@ var controller = {
                         status : true,
                         data:data
                     });
-        	} else {
-        		return defer.reject('没有任务了');
-        	}
+            } else {
+                return defer.reject('没有任务了');
+            }
         })
 
         return defer.promise;
@@ -386,4 +293,139 @@ var controller = {
         }, true)
         return defer.promise;
     }
+}
+
+//队列
+var dataList = {
+    storeArrList:[],
+    guoneiArrList:[],
+    guowaiArrList:[],
+    taobaoArrList:[],
+};
+
+//抓取标志
+var storeFlag;
+var guoneiFlag;
+var guowaiFlag;
+var taobaoFlag;
+
+//抓取时间
+var storeTime;
+var guoneiTime;
+var guowaiTime;
+var taobaoTime;
+
+//初始化获取队列
+var initialize = function(store){
+    if (store == '' || store == '京东'){
+        storeFlag = true;
+        storeTime = (new Date()).getTime();
+        controller.getDataEs('京东').then(function (data) {
+            dataList.storeArrList = data.data;
+            storeFlag = false;
+        },function (err) {
+            console.log('京东' + err)
+            storeFlag = false;
+        })
+    }
+
+    if (store == '' || store == 'guonei'){
+        guoneiFlag = true;
+        guoneiTime = (new Date()).getTime();
+        controller.getDataOtherEs('guonei').then(function (data) {
+            dataList.guoneiArrList = data.data;
+            guoneiFlag = false;
+        },function (err) {
+            console.log('guonei' + err)
+            guoneiFlag = false;
+        });
+    }
+
+    if (store == '' || store == 'guowai'){
+        guowaiFlag = true;
+        guowaiTime = (new Date()).getTime();
+        controller.getDataOtherEs('guowai').then(function (data) {
+            dataList.guowaiArrList = data.data;
+            guowaiFlag = false;
+        },function (err) {
+            console.log('guowai' + err)
+            guowaiFlag = false;
+        })
+    }
+
+    if (store == '' || store == 'taobao'){
+        taobaoFlag = true;
+        taobaoTime = (new Date()).getTime();
+        controller.getDataOtherEs('taobao').then(function (data) {
+            dataList.taobaoArrList = data.data;
+            taobaoFlag = false;
+        },function (err) {
+            console.log('taobao' + err)
+            taobaoFlag = false;
+        })
+    }
+}
+initialize('');
+
+
+//处理核心逻辑
+var handler = function (request, response){
+    var store = request.query.store;
+    //验证 必须传store
+    if (!store) {
+        response.json({code: 400, msg: '缺少store参数'});
+        return;
+    }
+
+    var resultList = [];
+    var resultFlag;
+    var resultTime;
+    if (store == '京东'){
+        resultList = dataList.storeArrList;
+        resultFlag = storeFlag;
+        resultTime = storeTime;
+    } else if(store == 'guonei'){
+        resultList = dataList.guoneiArrList;
+        resultFlag = guoneiFlag;
+        resultTime = guoneiTime;
+    } else if(store == 'guowai'){
+        resultList = dataList.guowaiArrList;
+        resultFlag = guowaiFlag;
+        resultTime = guowaiTime;
+    } else if(store == 'taobao'){
+        resultList = dataList.taobaoArrList;
+        resultFlag = taobaoFlag;
+        resultTime = taobaoTime;
+    }
+
+    if (resultList.length == 0){
+        if (!resultFlag || (new Date().getTime() - resultTime > 20 * 1000)){
+            initialize(store);
+        }
+        setTimeout(function(){
+            response.json({code: 400, msg: store+'没有任务了'});
+        },500);
+    } else {
+        var reslut = resultList.pop();
+        var redis_key = 'stone_get_crawl_task2_'+reslut.task_id;
+        //用redis的方式存储是否获取过，防止es更新缓慢造成重复抓取的问题
+        client.get(redis_key, function (err, reply) {
+            if (!reply){//没有获取过
+                //获取完把状态更新成1（处理中）
+                controller.updateDataEs(reslut.task_id).then(function (datas) {
+                    client.set(redis_key, 1);
+                    client.expire(redis_key, 10);
+                    response.json({code: 200, msg: '',data:reslut});
+                }, function (errs) {
+                    response.json({code: 400, msg: err.message});
+                })
+            } else {
+                //100ms后继续获取
+                setTimeout(function(){
+                    handler(request, response);
+                },50)
+            }
+        });
+    }
+    return;
 }
