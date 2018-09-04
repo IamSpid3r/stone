d7f31b639646dad32ef7517c3d5a364151188042
tang.xiaofei <tang.xiaofei@puscene.com>
2018-09-04 13:57:25 +0800

    tmall&jd crawl

--- shop.js
+++ shop.js
@@ -7,11 +7,13 @@ var taobao = require('./lib/shop/taobaoShop');
 var nikeStore = require('./lib/shop/nikeShop');
 var amazoncnStore = require('./lib/shop/amazoncnShop');
 var duStore = require('./lib/shop/duShop');
+var searchJdStore = require('./lib/shop/searchJdShop');
 var jdStore = require('./lib/shop/jdShop');
 var kaolaStore = require('./lib/shop/kaolaShop');
 var vipStore = require('./lib/shop/vipShop');
 var farfetch = require('./lib/shop/farfetchShop');
 var stockx = require('./lib/shop/stockxShop');
+var tmall = require('./lib/shop/tmallShop');
 
 app.get('/t',function(req,res){
     taobao.getInfo('https://auxdq.tmall.com',1,function(error,itemInfo){
@@ -25,13 +27,13 @@ app.get('/t',function(req,res){
 
 app.get('/info', function (req, res) {
     var goodsUrl = req.query.url;
-    var page = req.query.page ? req.query.page : 1;
+    var page = 1;
     var goodsUrlHost = '';
     if(goodsUrl){
         var urlInfo = url.parse(goodsUrl, true, true);
         goodsUrlHost = urlInfo.host;
+        page = req.query.page ?Number(req.query.page):Number(urlInfo.query.page);
     }
-    
     var storeObj = getStoreObj(goodsUrlHost);
     if(typeof storeObj == 'object'){
         storeObj.getInfo(goodsUrl , page, function(error, itemInfo){
@@ -59,24 +61,28 @@ app.get('/info', function (req, res) {
 
 //获取商城对象
 function getStoreObj(host){
-        if (host.indexOf('nike.com') > 0){
-            return nikeStore;
-        }else if(host.indexOf('amazon.cn') > 0){
-            return amazoncnStore;
-        }else if(host == 'du.hupu.com'){
-            return duStore;
-        }else if(host.indexOf('jd.com') > 0 || host.indexOf('jd.hk') > 0){
-            return jdStore;
-        }else if(host.indexOf('kaola.com') > 0){
-            return kaolaStore;
-        }else if(host.indexOf('vip.com') > 0){
-            return vipStore;
-        }else if(host.indexOf('farfetch.com') > 0){
-            return farfetch;
-        }else if(host.indexOf('stockx.com') >= 0){
-            return stockx;
-        }
-        return taobao;
+    if (host.indexOf('nike.com') > 0){
+        return nikeStore;
+    }else if(host.indexOf('amazon.cn') > 0){
+        return amazoncnStore;
+    }else if(host == 'du.hupu.com'){
+        return duStore;
+    }else if(host.indexOf('search.jd')>= 0){
+        return searchJdStore;
+    }else if(host.indexOf('jd.com') > 0 || host.indexOf('jd.hk') > 0){
+        return jdStore;
+    }else if(host.indexOf('kaola.com') > 0){
+        return kaolaStore;
+    }else if(host.indexOf('vip.com') > 0){
+        return vipStore;
+    }else if(host.indexOf('farfetch.com') > 0){
+        return farfetch;
+    }else if(host.indexOf('stockx.com') >= 0){
+        return stockx;
+    }else if(host.indexOf('list.tmall') >= 0){
+        return tmall;
+    }
+    return taobao;
 
 }
 
