da38d9267a3982ee365bc3610f0349170fb0b15c
tang.xiaofei <tang.xiaofei@puscene.com>
2018-10-30 12:20:51 +0800

    redis connect error

--- apps/lib/redis.js
+++ apps/lib/redis.js
@@ -3,11 +3,18 @@ const NODE_ENV = typeof process.env.NODE_ENV != 'undefined' ? process.env.NODE_E
 var config = require(process.cwd()+'/config/'+NODE_ENV+'/app.json');
 var redisConfig = config.db.redis;
 console.log(redisConfig);
-var client = redis.createClient(redisConfig.port,redisConfig.host);
-if (!NODE_ENV){
-    //线上环境需要认证
-    client.auth(redisConfig.password);
-}
+var client = redis.createClient({
+    host: redisConfig.host,
+    port: redisConfig.port,
+    db: redisConfig.database,
+    password: redisConfig.password,
+    connect_timeout: 5000,
+    socket_keepalive: true
+});
+// if (!NODE_ENV){
+//     //线上环境需要认证
+//     client.auth(redisConfig.password);
+// }
 
 client.on("error", function (err) {
     console.log("Error " + err);
