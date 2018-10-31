5b022bfdabd1d44008ce25c708ff70a9d15188c9
韩晓林 <hanxiaolin@hupu.com>
2018-10-31 19:54:06 +0800

    crawl new

--- apps/lib/redis.js
+++ apps/lib/redis.js
@@ -2,7 +2,6 @@ var redis = require('redis');
 const NODE_ENV = typeof process.env.NODE_ENV != 'undefined' ? process.env.NODE_ENV : '';
 var config = require(process.cwd()+'/config/'+NODE_ENV+'/app.json');
 var redisConfig = config.db.redis;
-console.log(redisConfig);
 var client = redis.createClient(redisConfig.port,redisConfig.host);
 if (!NODE_ENV){
     //线上环境需要认证
