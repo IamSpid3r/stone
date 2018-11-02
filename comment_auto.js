71c9920ac0b9711d3761621648a713c556a2b20c
gwyy <279861795@qq.com>
2018-11-02 16:38:37 +0800

    抓评论优化，每次返回1页而不是五页

--- comment_auto.js
+++ comment_auto.js
@@ -1,8 +1,6 @@
 var url = require('url');
 var request = require('request');
 //需要支持 淘宝|天猫 亚马逊  识货  京东 国美
-var taobao = require('./lib/comment/taobaoComment');
-var tbNew = require('./lib/comment/tbNewComment');  //淘宝新抓取 
 var tbPc = require('./lib/comment/tbPcComment');
 var amazonCn = require('./lib/comment/amazonCnComment');
 var shihuoHaitao = require('./lib/comment/shihuoHaitaoComment');
@@ -11,7 +9,6 @@ var jd = require('./lib/comment/jdComment');
 var kaola = require('./lib/comment/kaluli/kaolaComment');//考拉
 var mia = require('./lib/comment/kaluli/miaComment');//蜜芽
 const gwyy = require('./tools/gwyy');
-
 
 //错误 json 
 var err_info = {
@@ -23,148 +20,21 @@ var err_info = {
         }]
     }
 };
+
 //json
 var producerUrl = "http://www.shihuo.cn/api/stoneComment/act/producer?stone=all";
 var consumerUrl = "http://www.shihuo.cn/api/stoneComment/act/consumer";
 
 
-var stone = {
-    productData:null, //当前抓取链接信息
-    init:function(){
-        var that = this;
-
-        //拿到待抓取的渠道
-        that.producer(function(producter){
-            if(producter){
-                //调用抓取接口 
-                that.stoneApi(producter, function(formData){
-                    that.productData = null;
-                    //返回应答
-                    that.consumer(formData, function(){
-                        setTimeout(function(){ //work
-                            that.init();
-                        },2000)
-                    });
-                });
-            }else{ //如果没有连接 那么等会
-                console.log("waiting...");
-                setTimeout(function(){
-                    that.init();
-                },4000)
-            }
-        });
-    },
-
-
-    /**
-     * 从识货拿到需要抓取的连接
-     */
-    producer:function(callback){
-        /*
-        var a = { status: true,
-            data:  { id: '468546',url: 'https://detail.tmall.hk/hk/item.htm?id=40088519684' } };
-            callback(a); */
-   
-        var that = this;
-        if(that.productData){
-            callback(that.productData)
-        }else{
-            request(producerUrl, function(error,response,body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    if(body.status){
-                        console.log(body);
-                        that.productData = body;
-                        callback(body)
-                    }else{
-                        callback(false)
-                    }
-                }else{
-                    callback(false)
-                }
-            })
-        } 
-    },
-    /**
-     * 应答给识货
-     */
-    consumer:function(formData,callback){
-        console.log(JSON.stringify(formData,null,3));
-        request.post({
-            url: consumerUrl,
-            form: {'info': JSON.stringify(formData)},
-        },function(error,response,body){
-            console.log("识货返回"+body);
-            console.log("识货返回错误"+error);
-            callback(body,error);
-        });
-    },
-
-    /**
-     * 真正的抓取方法
-     */
-    stoneApi:function(body, callback){
-        var that = this;
-        var goodsUrl = body.data.url;
-        var goodsUrlHost = '';
-        
-        if(goodsUrl){
-            var urlInfo = url.parse(goodsUrl, true, true);
-            goodsUrlHost = urlInfo.host;
-        }
-        console.log(goodsUrl);
-        //抓取数据
-        var storeObj = [],
-        itemInfo = [];
-        storeObj = getStoreObj(goodsUrlHost);
-        if(typeof storeObj == 'object'){
-            Promise.race([getComment(storeObj,goodsUrl,body.data.id), timeout()])
-            .then(function(formData){  //如果20秒内返回了
-                callback(formData);
-                console.log("正常返回。。。。");
-            })
-            .catch(function(error){  //如果20秒内还没返回
-                console.log(error);
-                that.productData = null;
-                that.consumer({Status: 1, Id: body.data.id, Msg: ''}, function(){
-                    setTimeout(function(){ //work
-                        that.init();
-                    },4000)
-                });
-            });
-        }else{
-            var formData = {Status: 1, Id: body.data.id, Msg: "请求地址不在抓取访问"};
-            console.log(formData);
-            callback(formData);
-        }
-    }//stone end
-};
-
-
-function getComment(storeObj,goodsUrl,id) {
-    var p = new Promise(function(resolve, reject){
-        if(typeof storeObj == 'object'){
-            storeObj.getInfo(goodsUrl ,function(error, itemInfo){  
-                //如果出错了
-                if(error){
-                    console.log(error); 
-                    reject("error");
-                } else {
-                        //res.json({ Status: false,Msg: error});
-                        //var formData = {Status: 1, Id: body.data.id, Msg: error};
-                        //res.json({ Status: true, Data: itemInfo});
-                    var formData = { Status: 2, Id: id, Data: itemInfo};  
-                    resolve(formData);
-                }   
-            });
-        } else {
-            reject('请求地址不在抓取访问');
-        }
+async function timeout(time = 25000){
+    return  new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve('');
+        }, time);
     });
-    return p;
 }
 
-function timeout(){
+async function timeout_race(){
     var p = new Promise(function(resolve, reject){
         setTimeout(function(){
             reject('URL请求20秒无响应');
@@ -173,21 +43,166 @@ function timeout(){
     return p;
 }
 
-//获取商城对象
-function getStoreObj(host){
-    switch(host){
-        case 'item.taobao.com':
-        case 'detail.tmall.com':
-        case 'detail.tmall.hk':
-            return tbPc;
-        case 'item.gome.com.cn':
-            return gome;
-        case 'item.jd.com':
-        case 'item.jd.hk':
-            return jd;
-        default:
-            return '';
+var a= 1;
+class crawlOtherComment {
+
+    constructor() {
+        console.log("initializing.....");
     }
+    /**
+     * 返回给识货
+     * @param {*} formData 
+     */
+    
+    async  consumer(formData) {
+        console.log(JSON.stringify(formData,null,3));
+        return new Promise(function(resolve, reject){
+            request.post({
+                url: consumerUrl,
+                form: {'info': JSON.stringify(formData)},
+            },function(error,response,body){
+                console.log("识货返回"+body);
+                console.log("识货返回错误"+error);
+                resolve("success");
+            });
+        });
+    }
+
+    /**
+     * 开始抓取
+     */
+    async  init() {
+       while(true) {
+           try {
+            //从识货拿到需要抓取的url
+            let commentData = await  this.getComment();
+            let stoneObj = await this.getStone(commentData);
+            //如果不存在url  那么直接返回给识货
+            if(typeof stoneObj != 'object') {
+                var formData = {Status: 1, Id: commentData.data.id, Msg: "请求地址不在抓取访问"};
+                console.log(formData);
+                //直接返回识货
+                await this.consumer(formData);
+                continue;
+            }
+            //开始抓取 只抓5页
+            for(let i=1;i<=5;i++) {
+                await this.crawl(stoneObj,commentData,i);
+            }
+           } catch(e) {
+               console.log("ERROR:"+e);
+           }
+           await timeout(2500);
+           //return Promise.resolve();
+       }  // while  end
+    }
+
+
+    async  crawlComment(stoneObj,goodsUrl,index) {
+        return new Promise(function(resolve, reject){
+            stoneObj.getInfo(goodsUrl,index ,function(error, itemInfo){  
+                //如果出错了
+                if(error){
+                    console.log(error); 
+                    reject("error");
+                } else {
+                    resolve(itemInfo);
+                }   
+            });
+        });
+    }
+    /**
+     * 抓取函数
+     */
+    async  crawl(stoneObj,commentData,index) {
+        var that = this;
+        return new Promise(async function(resolve, reject){
+            Promise.race([that.crawlComment(stoneObj,commentData.data.url,index), timeout_race()])
+            .then(async function(itemInfo){  //返回成功
+                var formData = { Status: 2, Id: commentData.data.id, Data: itemInfo};  
+                await that.consumer(formData);
+                resolve();
+            })
+            .catch(async function(error){  //如果20秒内还没返回
+                console.log(error);
+                var formData = {Status: 1, Id: commentData.data.id, Msg: ''};
+                //直接返回识货
+                await that.consumer(formData);
+                resolve();
+            });
+        });
+    }
+
+
+    /**
+     * 获取商城
+     */
+    async  getStone(commentData) {
+        return new Promise(function(resolve, reject){
+            let goodsUrl = commentData.data.url;
+            if(goodsUrl == undefined || goodsUrl == "") reject("url 不可以为空！");
+            let urlInfo = url.parse(goodsUrl, true, true);
+            let goodsUrlHost = urlInfo.host;
+            switch(goodsUrlHost){
+                case 'item.taobao.com':
+                case 'detail.tmall.com':
+                case 'detail.tmall.hk':
+                     resolve(tbPc);
+                case 'item.gome.com.cn':
+                    resolve(gome);
+                case 'item.jd.com':
+                case 'item.jd.hk':
+                    resolve(jd);
+                default:
+                    resolve();
+            }
+        });
+    }
+
+
+   
+
+
+
+    //获取 识货的内容
+    async  getComment() {
+        if(this.productData)  return;
+        var that = this;
+        return new Promise(function(resolve, reject){
+            request(producerUrl,function(error,response,body) {
+                if (!error && response.statusCode == 200) {
+                    body = JSON.parse(body);
+                    if(body.status){
+                        console.log(body);
+                        resolve(body);
+                    }else{
+                        reject("读取识货失败1");
+                    }
+                }else{
+                    reject("读取识货失败2");
+                }
+            })
+        });
+      
+    }
+
+
+
+
 }
-//start
-stone.init(1);
+
+
+
+
+
+(async function(){
+
+    let obj = new crawlOtherComment();
+    await obj.init();
+   
+
+})();
+
+
+
+
